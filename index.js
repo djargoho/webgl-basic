9975dd6d649b972a87b883010078d00fd09a8241
Mordiumaco <mordiumaco@gmail.com>
2019-10-30 22:50:15 +0900

    test commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import test from "./src/test";
 
-
 console.log(test);
 const appElement = document.getElementById("app");
 
-appElement.append("content1 아까는 안됐잖아!");
+appElement.append("content1 아까는 안됐잖아!! Fucking!");
